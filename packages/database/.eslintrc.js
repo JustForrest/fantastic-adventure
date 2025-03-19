6ff3c4f2afadf9b64a3a1ff8d9e2c57229861e30
Forrest <879171+JustForrest@users.noreply.github.com>
2025-03-18 21:10:39 -0400

    Update dependencies in package.json files across multiple apps and add ESLint configuration for the database package. Upgraded Fastify to 4.29.0, Next.js to 14.2.25, and Typesense to 1.8.2. Adjusted linting command in database package.json for TypeScript files. Added .eslintrc.js for database package to enforce coding standards.

---
+++ packages/database/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+}; 
